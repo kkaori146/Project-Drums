9b26c520dbbcc53766f5a86cdbccb80421f8e0c7
Kaminishikawahara KK <kkaori146@gmail.com>
2021-09-10 16:38:15 +0900

    Project Completed

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.body.addEventListener('keyup', (event)=>{
     playSound(event.code.toLowerCase());
 });
+
+document.querySelector('.composer button').addEventListener('click', ()=>{
+    let song = document.querySelector('#input').value;
+
+    if (song !== '') {
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
+});
+
     function playSound(sound) {
         let audioElement = document.querySelector(`#s_${sound}`);
         let keyElement = document.querySelector(`div[data-key="${sound}"]`);
@@ -15,5 +25,17 @@ document.body.addEventListener('keyup', (event)=>{
             setTimeout(()=>{
                 keyElement.classList.remove('active');
             }, 300);
+        }
+    }
+
+    function playComposition(songArray) {
+        let wait = 0;
+
+        for(let songItem of songArray) {
+                setTimeout(()=>{
+                    playSound(`key${songItem}`);
+                }, wait);
+
+                wait += 250;   
         }
     }
